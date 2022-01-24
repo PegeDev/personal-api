d5055e66895dd037759137532bd5f33ff3188910
PegeDev <89503020+PegeDev@users.noreply.github.com>
2022-01-25 00:14:14 +0700

    Update tik.js

--- server/routers/tik.js
+++ server/routers/tik.js
@@ -7,18 +7,18 @@ async function getVideo(URL) {
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     })
     const page = await browser.newPage()
-    await page.goto("https://musicaldown.com/id")
+    await page.goto("https://musicaldown.com/")
     await page.type('#link_url', `${URL}`)
     await page.click('#submit-form > div > div:nth-child(2) > button', {delay: 300})
 
-//     await page.waitForSelector("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l4 > img")
-//     let img = await page.$eval("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l4 > img", (element) => {
-//         return element.getAttribute("src")
-//     })
-//     await page.waitForSelector("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(4)")
-//     let mp4server1 = await page.$eval("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(4)", (element) => {
-//         return element.getAttribute("href")
-//     })
+     await page.waitForSelector("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l4 > img")
+    let img = await page.$eval("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l4 > img", (element) => {
+        return element.getAttribute("src")
+    })
+    await page.waitForSelector("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(4)")
+    let mp4server1 = await page.$eval("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(4)", (element) => {
+        return element.getAttribute("href")
+    })
     await page.waitForSelector("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(6)")
     let mp4server2 = await page.$eval("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(6)", (element) => {
         return element.getAttribute("href")
